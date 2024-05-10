bd233bfee6faff61324fb2cf8f0934aa77cf9013
Osborn.Jhin <105868544+OsbornJhin@users.noreply.github.com>
2024-05-10 11:22:19 -0300

    Update main.js

--- main.js
+++ main.js
@@ -37,14 +37,14 @@ function calculaTempo(tempoObjetivo) {
     if (tempoFinal > 0){
         return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
     } else {
-        return "Prazo Finalizado";
+        return [0,0,0,0];
     }
 }
 
 function atualizaCronometro(){
-       
+    document.getElementById("dias0").calculaTempo(tempos[0]);   
     for (let i=0; i<contadores.length;i++){
-        contadores[i].textContent = calculaTempo(tempos[i]);   
+        //contadores[i].textContent = calculaTempo(tempos[i]);   
     }
 }
 
